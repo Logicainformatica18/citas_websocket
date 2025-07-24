1cbf3b65ba69adbe0addeff7a3315ffe27eaf634
logicainformatica18 <logicainformatica18gmail.com>
2025-07-24 14:37:37 -0500

    quitando log

--- app/Notifications/NewSupportAtcNotification.php
+++ app/Notifications/NewSupportAtcNotification.php
@@ -21,7 +21,7 @@ class NewSupportAtcNotification extends Notification implements ShouldQueue
 
         // Carga todas las relaciones necesarias
         $this->support = $support->loadMissing([
-            'client:id_cliente,Razon_Social,Telefono,Email,Direccion',
+            'client:id_cliente,Razon_Social,Telefono,Email,Direccion,dni',
             'creator:id,firstname,lastname,names,email',
             'details:id,support_id,subject,description,priority,type,status,reservation_time,attended_at,derived,Manzana,comment,attachment,project_id,area_id,id_motivos_cita,id_tipo_cita,id_dia_espera,internal_state_id,external_state_id,type_id,ticket,channel',
             'details.area:id_area,descripcion',
