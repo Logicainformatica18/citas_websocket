8f9419851d2da92ff7a2ce2fa539481463fbf028
Anthony isil <acardenas@isil.pe>
2026-03-17 17:19:31 -0500

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,7 +33,7 @@
         </style>
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
+<link rel="icon" href="/logo/isil_logo.jpg">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
